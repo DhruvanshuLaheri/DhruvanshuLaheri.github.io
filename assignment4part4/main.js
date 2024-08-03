3cbc92dd46a84a5858da42ebc5241822d61b95bc
DhruvanshuLaheri <dhruvanshu.laheri@dcmail.ca>
2024-08-03 14:14:40 -0400

    commit 3
    const added

--- assignment4part4/main.js
+++ assignment4part4/main.js
@@ -1,15 +1,19 @@
 // set up canvas
 
+const para = document.querySelector('p');
+let count = 0;
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 
 // function to generate random number
 
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
 }
 
 // function to generate random RGB color value
